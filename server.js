870aea81cf459b14394f81f2fab8d61b4ba1df83
Andhre Cruz <138709296+AndhreCruz@users.noreply.github.com>
2024-11-27 15:25:28 -0300

    more phrases added

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('phrases.json');
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server'); // Importa JSON Server
+const server = jsonServer.create(); // Crea una instancia de JSON Server
+const router = jsonServer.router('phrases.json'); // Crea un router usando el archivo JSON
+const middlewares = jsonServer.defaults(); // Carga middlewares predeterminados (logger, static, cors y no-cache)
 
 // Usa el puerto proporcionado por Railway
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Define el puerto en el que se ejecutará el servidor
 
-server.use(middlewares);
-server.use(router);
+server.use(middlewares); // Carga middlewares
+server.use(router); // Carga el router
 
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+server.listen(port, () => { // Inicia el servidor
+  console.log(`JSON Server is running on port ${port}`); // Muestra un mensaje en la consola
 });
